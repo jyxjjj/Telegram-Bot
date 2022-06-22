b590012e4bda2e8186850f0a62d66a80b3ff6c51
jyxjjj <773933146@qq.com>
2022-06-22 20:19:11 +0800

    Create init.sh

---
+++ supervisor/init.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp /www/wwwroot/TelegramBot/supervisor/TelegramBot-Queue-default.conf /etc/supervisor/conf.d/
+cp /www/wwwroot/TelegramBot/supervisor/TelegramBot-Queue-TelegramLimitedApiRequest.conf /etc/supervisor/conf.d/
