41798abf0c70aa0c15244ae4dd6fbd14720820ee
jyxjjj <773933146@qq.com>
2022-06-26 01:15:18 +0800

    Update IndexController.php

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Common\DESMG;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class IndexController extends BaseController
 {
@@ -11,5 +13,11 @@ class IndexController extends BaseController
     {
         dd(DESMG::about([], 123));
         dd(request()->server('HTTP_CF_CONNECTING_IP'), request()->ip());
+    }
+
+    public function webhook(Request $request): JsonResponse
+    {
+        Log::debug('webhook', [$request->all(), $request->server('HTTP_X_TELEGRAM_BOT_API_SECRET_TOKEN')]);
+        return response()->json(['status' => 'ok']);
     }
 }
