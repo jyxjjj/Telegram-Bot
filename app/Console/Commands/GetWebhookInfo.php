62ec35e686a857c15ec15ad544cda90bac4aa860
jyxjjj <773933146@qq.com>
2022-06-26 02:32:54 +0800

    Update GetWebhookInfo.php

--- app/Console/Commands/GetWebhookInfo.php
+++ app/Console/Commands/GetWebhookInfo.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Longman\TelegramBot\Entities\WebhookInfo;
 use Longman\TelegramBot\Exception\TelegramException;
 use Longman\TelegramBot\Request;
 use Longman\TelegramBot\Telegram;
@@ -20,7 +21,25 @@ class GetWebhookInfo extends Command
             if (!$request->isOk()) {
                 throw new TelegramException($request->getDescription());
             }
-            self::info(json_encode($request->getResult(), JSON_PRETTY_PRINT));
+            /** @var $result WebhookInfo */
+            $result = $request->getResult();
+            $url = $result->getUrl() == '' ? 'Not set' : $result->getUrl();
+            $ip = $result->getIpAddress() == '' ? '<empty>' : $result->getIpAddress();
+            $has_custom_certificate = $result->getHasCustomCertificate() ? 'true' : 'false';
+            $pending_update_count = $result->getPendingUpdateCount();
+            $last_error_date = $result->getLastErrorDate() == '' ? '<empty>' : $result->getLastErrorDate();
+            $last_error_message = $result->getLastErrorMessage() == '' ? '<empty>' : $result->getLastErrorMessage();
+            $max_connections = $result->getMaxConnections() == '' ? '40' : $result->getMaxConnections();
+            $allowed_updates = count($result->getAllowedUpdates()) == 0 ? '<empty>' : ' - ' . implode("\n - ", $result->getAllowedUpdates());
+            self::info("URL: $url");
+            self::info("IP: $ip");
+            self::info("Has custom certificate: $has_custom_certificate");
+            self::info("Pending update count: $pending_update_count");
+            self::info("Last error date: $last_error_date");
+            self::info("Last error message: $last_error_message");
+            self::info("Max connections: $max_connections");
+            self::info('Allowed updates: ');
+            self::info($allowed_updates);
         } catch (TelegramException $e) {
             self::error($e->getMessage());
         }
