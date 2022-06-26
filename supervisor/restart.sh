b39122cf701ee9f61082d20fe7f827725d0d5042
jyxjjj <773933146@qq.com>
2022-06-26 22:32:59 +0800

    Update restart.sh

--- supervisor/restart.sh
+++ supervisor/restart.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 supervisorctl reread
 supervisorctl update
+supervisorctl stop TelegramBot-Queue-default:*
+supervisorctl stop TelegramBot-Queue-TelegramLimitedApiRequest:*
 supervisorctl start TelegramBot-Queue-default:*
 supervisorctl start TelegramBot-Queue-TelegramLimitedApiRequest:*
