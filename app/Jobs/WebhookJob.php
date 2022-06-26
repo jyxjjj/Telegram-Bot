daa2403b5846754f353e160c5dc7e69535ff42a6
jyxjjj <773933146@qq.com>
2022-06-26 20:45:57 +0800

    Update WebhookJob.php

--- app/Jobs/WebhookJob.php
+++ app/Jobs/WebhookJob.php
@@ -4,17 +4,20 @@ namespace App\Jobs;
 
 use App\Common\Client;
 use App\Http\Services\UpdateHandleService;
+use Carbon\Carbon;
 use Longman\TelegramBot\Entities\Update;
 use Longman\TelegramBot\Exception\TelegramException;
 
 class WebhookJob extends BaseQueue
 {
     private array $data;
+    private Carbon $now;
 
-    public function __construct(array $data)
+    public function __construct(array $data, Carbon $now)
     {
         parent::__construct();
         $this->data = $data;
+        $this->now = $now;
     }
 
     /**
@@ -29,6 +32,7 @@ class WebhookJob extends BaseQueue
         $telegram->setUploadPath(storage_path('app/telegram'));
         $telegram->setCommandsPath(app_path('Http/Services/Commands'));
         $update = new Update($data, $telegram->getBotUsername());
+        define('COMMAND_START', $this->now->getTimestampMs());
         $telegram->processUpdate($update);
         UpdateHandleService::handle($update, $telegram);
     }
