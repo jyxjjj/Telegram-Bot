8068d40b24fa5d1003bccae8add28c43bc281dff
jyxjjj <773933146@qq.com>
2022-06-26 19:49:45 +0800

    plain

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Common\Config;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
 
 class BaseController extends Controller
@@ -13,5 +15,10 @@ class BaseController extends Controller
     final protected function json(array $data = []): JsonResponse
     {
         return response()->json($data);
+    }
+
+    final protected function plain($str): Response
+    {
+        return response($str, 200, Config::PLAIN_HEADER);
     }
 }
