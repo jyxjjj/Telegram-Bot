4da88c205e5f625c6780e20483bf01f25915fe02
jyxjjj <773933146@qq.com>
2022-07-01 18:55:16 +0800

    admin && private

--- app/Http/Services/CommandHandleService.php
+++ app/Http/Services/CommandHandleService.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Services;
 
+use App\Jobs\SendMessageJob;
 use Longman\TelegramBot\Entities\Message;
 use Longman\TelegramBot\Telegram;
 use RecursiveDirectoryIterator;
@@ -18,6 +19,11 @@ class CommandHandleService extends BaseService
      */
     public static function handle(Message $message, Telegram $telegram, int $updateId): void
     {
+        $isAdmin = !$telegram->isAdmin($message->getFrom()->getId());
+        $notPrivate = !$message->getChat()->isPrivateChat();
+        $sendCommand = $message->getCommand();
+        $chatId = $message->getChat()->getId();
+        $messageId = $message->getMessageId();
         $path = app_path('Http/Services/Commands');
         $files = new RegexIterator(
             new RecursiveIteratorIterator(
@@ -26,16 +32,44 @@ class CommandHandleService extends BaseService
             '/^.+Command.php$/'
         );
         foreach ($files as $file) {
-            $command = str_replace('.php', '', $file->getFileName());
+            $fileName = $file->getFileName();
+            $pathName = $file->getPathName();
+            $command = str_replace('.php', '', $fileName);
             $command_class = "App\\Http\\Services\\Commands\\$command";
-            require_once $file->getPathName();
-            if (class_exists($command_class, false)) {
-                $command_class = new $command_class($message, $telegram, $updateId);
-                if ($command_class->name == $message->getCommand()) {
-                    $command_class->execute($message, $telegram, $updateId);
-                    return;
-                }
+            require_once $pathName;
+            if (!class_exists($command_class, false)) {
+                continue;
             }
+            $command_class = new $command_class; // instantiate the command
+            if ($command_class->name != $sendCommand) { // Detect if command matches
+                continue;
+            }
+            if ($command_class->admin && $isAdmin) {// Detect if command is admin only
+                $data = [
+                    'chat_id' => $chatId,
+                    'parse_mode' => 'Markdown',
+                    'disable_web_page_preview' => true,
+                    'allow_sending_without_reply' => true,
+                    'reply_to_message_id' => $messageId,
+                    'text' => 'This command is admin only',
+                ];
+                dispatch(new SendMessageJob($data));
+                return;
+            }
+            if ($command_class->private && $notPrivate) {// Detect if command is private only
+                $data = [
+                    'chat_id' => $chatId,
+                    'parse_mode' => 'Markdown',
+                    'disable_web_page_preview' => true,
+                    'allow_sending_without_reply' => true,
+                    'reply_to_message_id' => $messageId,
+                    'text' => 'This command needs to be sent in a private chat.',
+                ];
+                dispatch(new SendMessageJob($data));
+                return;
+            }
+            $command_class->execute($message, $telegram, $updateId); // Execute command
+            return;
         }
     }
 }
