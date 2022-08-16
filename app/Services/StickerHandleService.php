e3d7f066e8f2a1d4fb9d16fabf5fb4c067ea83cf
jyxjjj <773933146@qq.com>
2022-08-16 21:27:10 +0800

    Update StickerHandleService.php

--- app/Services/StickerHandleService.php
+++ app/Services/StickerHandleService.php
@@ -2,9 +2,23 @@
 
 namespace App\Services;
 
+//use App\Jobs\DownloadStickerJob;
 use App\Services\Base\BaseService;
+use Longman\TelegramBot\Entities\Message;
+use Longman\TelegramBot\Telegram;
 
 class StickerHandleService extends BaseService
 {
-
+    /**
+     * @param Message $message
+     * @param Telegram $telegram
+     * @param int $updateId
+     * @return bool
+     */
+    public function handle(Message $message, Telegram $telegram, int $updateId): bool
+    {
+        $fileId = $message->getSticker()->getFileId();
+//        $this->dispatch(new DownloadStickerJob(['file_id' => $fileId,]));
+        return false;
+    }
 }
