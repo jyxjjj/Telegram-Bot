b4437f2e669b7117cc25e36356c367b5de263af6
jyxjjj <773933146@qq.com>
2022-08-23 14:32:12 +0800

    fix admin rights

--- app/Services/Commands/WarnCommand.php
+++ app/Services/Commands/WarnCommand.php
@@ -5,6 +5,7 @@ namespace App\Services\Commands;
 use App\Jobs\BanMemberByWarnJob;
 use App\Jobs\DeleteMessageJob;
 use App\Jobs\SendMessageJob;
+use App\Models\TChatAdmins;
 use App\Models\TChatWarns;
 use App\Services\Base\BaseCommand;
 use Longman\TelegramBot\Entities\Message;
@@ -36,6 +37,24 @@ class WarnCommand extends BaseCommand
         $chatType = $message->getChat()->getType();
         if (!in_array($chatType, ['group', 'supergroup'], true)) {
             $data['text'] .= "*Error:* This command is available only for groups.\n";
+            $this->dispatch(new SendMessageJob($data));
+            return;
+        }
+        $admins = TChatAdmins::getChatAdmins($chatId);
+
+        $userId = $message->getFrom()->getId();
+        if (!in_array($userId, $admins, true)) {
+            $data['text'] .= "*Error:* You should be an admin of this chat to use this command.\n\n";
+            $data['text'] .= "*Warning:* This command can be used by people who was an admin before update admin list.\n\n";
+            $data['text'] .= "*Notice:* Send /updatechatadministrators to update chat admin list.\n\n";
+            $this->dispatch(new SendMessageJob($data));
+            return;
+        }
+
+        $replyTo = $message->getReplyToMessage();
+        $warnUserId = $replyTo->getFrom()->getId();
+        if (in_array($warnUserId, $admins, true)) {
+            $data['text'] .= "*Error:* You can't warn an admin.\n";
             $this->dispatch(new SendMessageJob($data));
             return;
         }
