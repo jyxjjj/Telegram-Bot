c95720c0e1278b0571a7b42a197e328c396900fa
jyxjjj <773933146@qq.com>
2022-09-05 19:24:37 +0800

    fix

--- config/cache.php
+++ config/cache.php
@@ -8,7 +8,7 @@ return [
         ],
         'redis' => [
             'driver' => 'redis',
-            'connection' => 'default',
+            'connection' => 'cache',
         ],
     ],
     'prefix' => 'cache',
