b2c8d37d9ced9592d1897ea129b6797b27c43d8b
jyxjjj <773933146@qq.com>
2022-09-08 17:52:40 +0800

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,8 +2,12 @@
 
 namespace App\Console;
 
+use App\Console\Schedule\BilibiliSubscribe;
+use App\Console\Schedule\ChromeUpdateSubscribe;
+use App\Console\Schedule\LogClean;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Queue\Console\PruneFailedJobsCommand;
 
 class Kernel extends ConsoleKernel
 {
@@ -12,14 +16,14 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('queue:prune-failed --hours 72')->dailyAt('00:00')->runInBackground()->withoutOverlapping(120);
-        $schedule->command('log:clean 3')->hourly()->runInBackground()->withoutOverlapping(120);
-        $schedule->command('subscribe:bilibili')->everyTenMinutes()->runInBackground()->withoutOverlapping(120);
-        $schedule->command('subscribe:chrome')->hourly()->runInBackground()->withoutOverlapping(120);
+        $schedule->command(PruneFailedJobsCommand::class, ['--hours=72'])->dailyAt('00:00')->runInBackground()->withoutOverlapping(120);
+        $schedule->command(LogClean::class, ['3'])->hourly()->runInBackground()->withoutOverlapping(120);
+        $schedule->command(ChromeUpdateSubscribe::class)->hourly()->runInBackground()->withoutOverlapping(120);
 //        $schedule->command('subscribe:php')->hourly()->runInBackground()->withoutOverlapping(120);
 //        $schedule->command('subscribe:nginx')->hourly()->runInBackground()->withoutOverlapping(120);
 //        $schedule->command('subscribe:mariadb')->hourly()->runInBackground()->withoutOverlapping(120);
 //        $schedule->command('subscribe:redis')->hourly()->runInBackground()->withoutOverlapping(120);
+        $schedule->command(BilibiliSubscribe::class)->everyTenMinutes()->runInBackground()->withoutOverlapping(120);
     }
 
     protected function commands()
