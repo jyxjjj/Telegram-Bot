b23fdbc034ae1a1912c1627eb024128675e9d9f3
jyxjjj <773933146@qq.com>
2022-10-08 14:07:40 +0800

    Update SoftwareInterface.php

--- app/Console/Schedule/WellKnownSoftwareUpdateSubscribe/SoftwareInterface.php
+++ app/Console/Schedule/WellKnownSoftwareUpdateSubscribe/SoftwareInterface.php
@@ -2,7 +2,25 @@
 
 namespace App\Console\Schedule\WellKnownSoftwareUpdateSubscribe;
 
+use JetBrains\PhpStorm\ArrayShape;
+use Longman\TelegramBot\Entities\InlineKeyboard;
+
 interface SoftwareInterface
 {
+    /**
+     * @return string
+     */
     public function getVersion(): string;
+
+    /**
+     * @param int $chat_id
+     * @param string $version
+     * @return array
+     */
+    #[ArrayShape([
+        'chat_id' => 'int',
+        'text' => 'string',
+        'reply_markup' => InlineKeyboard::class,
+    ])]
+    public function generateMessage(int $chat_id, string $version): array;
 }
