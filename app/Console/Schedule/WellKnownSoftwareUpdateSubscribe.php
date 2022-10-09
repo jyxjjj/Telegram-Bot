fcde9267988c5911c34575fdfdbfe2c26396e5c6
jyxjjj <773933146@qq.com>
2022-10-09 10:00:54 +0800

    Nginx

--- app/Console/Schedule/WellKnownSoftwareUpdateSubscribe.php
+++ app/Console/Schedule/WellKnownSoftwareUpdateSubscribe.php
@@ -2,8 +2,10 @@
 
 namespace App\Console\Schedule;
 
+use App\Console\Schedule\WellKnownSoftwareUpdateSubscribe\Common;
 use App\Console\Schedule\WellKnownSoftwareUpdateSubscribe\Software;
 use App\Exceptions\Handler;
+use App\Jobs\SendMessageJob;
 use App\Models\TUpdateSubscribes;
 use Illuminate\Console\Command;
 use Illuminate\Foundation\Bus\DispatchesJobs;
@@ -35,17 +37,20 @@ class WellKnownSoftwareUpdateSubscribe extends Command
 //                    }
 //                }
             foreach (Software::cases() as $software) {
+                if (!in_array($software->name, [Software::PHP->name, Software::Nginx->name])) {
+                    continue;
+                }
                 /** @var TUpdateSubscribes[] $datas */
                 $datas = TUpdateSubscribes::getAllSubscribeBySoftware($software->name);
                 foreach ($datas as $data) {
-                    $chat_id = $data->chat_id;
-//                    $version = $software->getVersion();
-//                    $lastVersion = $this->getLastVersion($software);
-//                    if ($lastVersion != $version) {
-//                        $message = $software->getMessage($version);
-//                        $this->dispatch(new SendMessageJob($message, null, 0));
-//                        $this->setLastVersion($software, $version);
-//                    }
+                    $chat_id = $data['chat_id'];
+                    $version = $software->getInstance()->getVersion();
+                    $lastVersion = Common::getLastVersion($software);
+                    if ($version && $lastVersion != $version) {
+                        $message = $software->getInstance()->generateMessage($chat_id, $version);
+                        $this->dispatch(new SendMessageJob($message, null, 0));
+                        Common::setLastVersion($software, $version);
+                    }
                 }
             }
             return self::SUCCESS;
--- app/Console/Schedule/WellKnownSoftwareUpdateSubscribe/Softwares/Nginx.php
+++ app/Console/Schedule/WellKnownSoftwareUpdateSubscribe/Softwares/Nginx.php
@@ -2,18 +2,82 @@
 
 namespace App\Console\Schedule\WellKnownSoftwareUpdateSubscribe\Softwares;
 
+use App\Common\Config;
+use App\Console\Schedule\WellKnownSoftwareUpdateSubscribe\Common;
+use App\Console\Schedule\WellKnownSoftwareUpdateSubscribe\Software;
 use App\Console\Schedule\WellKnownSoftwareUpdateSubscribe\SoftwareInterface;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Http;
 use JetBrains\PhpStorm\ArrayShape;
 use Longman\TelegramBot\Entities\InlineKeyboard;
+use Longman\TelegramBot\Entities\InlineKeyboardButton;
 
 class Nginx implements SoftwareInterface
 {
-
+    /**
+     * @return string
+     */
     public function getVersion(): string
     {
-        // TODO: Implement getVersion() method.
+        $data = $this->getJson();
+        if (!is_array($data)) {
+            return Common::getLastVersion(Software::PHP);
+        }
+        $major = 0;
+        $minor = 0;
+        $patch = 0;
+        foreach ($data as $branch) {
+            if (preg_match('/^release-(\d+)\.(\d+)\.(\d+)$/i', $branch['name'], $matches)) {
+                if ($matches[1] > $major) {
+                    $major = $matches[1];
+                    $minor = $matches[2];
+                    $patch = $matches[3];
+                } elseif ($matches[1] == $major && $matches[2] > $minor) {
+                    $minor = $matches[2];
+                    $patch = $matches[3];
+                } elseif ($matches[1] == $major && $matches[2] == $minor && $matches[3] > $patch) {
+                    $patch = $matches[3];
+                }
+            }
+        }
+        $version = "{$major}.{$minor}.{$patch}";
+        Common::setLastVersion(Software::PHP, $version);
+        return $version;
     }
 
+    /**
+     * @return array|int|false
+     */
+    private function getJson(): array|int|false
+    {
+        $headers = Config::CURL_HEADERS;
+        $ts = Carbon::now()->getTimestamp();
+        $headers['User-Agent'] .= "; Telegram-Nginx-Subscriber-Runner/$ts";
+        $last_modified = Common::getLastModified(Software::PHP);
+        if ($last_modified) {
+            $headers['If-Modified-Since'] = $last_modified;
+        }
+        $get = Http::
+        withHeaders($headers)
+            ->accept('application/vnd.github+json')
+            ->withToken(env('GITHUB_TOKEN'))
+            ->get('https://api.github.com/repos/nginx/nginx/tags?per_page=100');
+        $last_modified = $get->header('last-modified');
+        Common::cacheLastModified(Software::PHP, $last_modified);
+        if ($get->status() == 200) {
+            return $get->json();
+        }
+        if ($get->status() == 304) {
+            return 304;
+        }
+        return false;
+    }
+
+    /**
+     * @param int $chat_id
+     * @param string $version
+     * @return array
+     */
     #[ArrayShape([
         'chat_id' => 'int',
         'text' => 'string',
@@ -21,6 +85,21 @@ class Nginx implements SoftwareInterface
     ])]
     public function generateMessage(int $chat_id, string $version): array
     {
-        // TODO: Implement generateMessage() method.
+        $emoji = Common::emoji();
+        $message = [
+            'chat_id' => $chat_id,
+            'text' => "{$emoji} A new version of Nginx($version) is now available.",
+            'reply_markup' => new InlineKeyboard([]),
+        ];
+        $button1 = new InlineKeyboardButton([
+            'text' => 'View',
+            'url' => 'https://nginx.org/en/download.html',
+        ]);
+        $button2 = new InlineKeyboardButton([
+            'text' => 'Download',
+            'url' => "https://nginx.org/download/nginx-{$version}.tar.gz",
+        ]);
+        $message['reply_markup']->addRow($button1, $button2);
+        return $message;
     }
 }
