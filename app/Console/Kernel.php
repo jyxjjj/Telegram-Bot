a2392cfe8ef456c0efc8de723b04266235b84f97
jyxjjj <773933146@qq.com>
2022-10-09 16:17:44 +0800

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use App\Console\Schedule\ChromeUpdateSubscribe;
 use App\Console\Schedule\LogClean;
+use App\Console\Schedule\WellKnownSoftwareUpdateSubscribe;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Illuminate\Queue\Console\PruneFailedJobsCommand;
@@ -18,7 +19,7 @@ class Kernel extends ConsoleKernel
         $schedule->command(PruneFailedJobsCommand::class, ['--hours=72'])->dailyAt('00:00')->runInBackground()->withoutOverlapping(120);
         $schedule->command(LogClean::class, ['3'])->hourly()->runInBackground()->withoutOverlapping(120);
         $schedule->command(ChromeUpdateSubscribe::class)->hourly()->runInBackground()->withoutOverlapping(120);
-//        $schedule->command(WellKnownSoftwareUpdateSubscribe::class)->hourly()->runInBackground()->withoutOverlapping(120);
+        $schedule->command(WellKnownSoftwareUpdateSubscribe::class)->hourly()->runInBackground()->withoutOverlapping(120);
 //        $schedule->command(BilibiliSubscribe::class)->hourly()->runInBackground()->withoutOverlapping(120);
     }
 
