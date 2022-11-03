d95c0110cca5f571f3b8daf1dbde3e38fb69ba28
jyxjjj <773933146@qq.com>
2022-11-03 15:25:23 +0800

    database

--- database/migrations/2022_11_03_143957_create_chat_keywords_table.php
+++ database/migrations/2022_11_03_143957_create_chat_keywords_table.php
@@ -11,17 +11,18 @@ return new class extends Migration {
         Schema::create('chat_keywords', function (Blueprint $table) {
             $table->unsignedBigInteger('id')->autoIncrement()->comment('主键');
             $table->BigInteger('chat_id')->default(0)->comment('聊天ID');
+            $table->BigInteger('user_id')->default(0)->comment('用户ID');
+            $table->BigInteger('from_chat_id')->default(0)->comment('来源聊天ID');
             $table->string('keyword', 128)->default('')->comment('关键字');
-            $table->enum('operation', ['REPLY', 'DELETE', 'BAN', 'RESTRICT'])->default('DELETE')->comment('执行操作');
-            $table->json('data')->default(DB::raw('JSON_OBJECT()'))->comment('数据');
+            $table->enum('detect_source', ['USERID', 'NAME', 'CHATID', 'TITLE', 'TEXT'])->default('TEXT')->comment('检测目标');
+            $table->enum('operation', ['REPLY', 'DELETE', 'WARN', 'BAN', 'RESTRICT'])->default('DELETE')->comment('执行操作');
+            $table->json('data')->default(DB::raw('JSON_OBJECT()'))->comment('操作数据');
             $table->boolean('enable')->default(1)->comment('启用');
             $table->timestamp('created_at')->useCurrent()->comment('创建时间');
             $table->timestamp('updated_at')->useCurrent()->comment('更新时间');
             $table->timestamp('deleted_at')->nullable()->comment('删除时间');
             $table->comment('聊天关键字表');
             $table->index(['chat_id',], 'chat_id');
-            $table->index(['keyword',], 'keyword');
-            $table->index(['operation',], 'operation');
             $table->engine = 'InnoDB';
             $table->charset = 'utf8mb4';
             $table->collation = 'utf8mb4_general_ci';
