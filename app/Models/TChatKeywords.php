1085804d26dc970eb6374d794a0af39a90c610cf
jyxjjj <773933146@qq.com>
2022-11-23 11:13:45 +0800

    KeywordDetectKeyword

--- app/Models/TChatKeywords.php
+++ app/Models/TChatKeywords.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Cache;
 
@@ -9,8 +11,8 @@ use Illuminate\Support\Facades\Cache;
  * @property int $id
  * @property int $chat_id
  * @property string $keyword
- * @property string $target
- * @property string $operation
+ * @property TChatKeywordsTargetEnum $target
+ * @property TChatKeywordsOperationEnum $operation
  * @property array $data
  * @property int $enable
  */
@@ -18,27 +20,26 @@ class TChatKeywords extends BaseModel
 {
     protected $table = 'chat_keywords';
     protected $casts = [
+        'data' => 'array',
         'target' => TChatKeywordsTargetEnum::class,
         'operation' => TChatKeywordsOperationEnum::class,
-        'data' => 'array',
     ];
 
     /**
      * @param int $chat_id
-     * @return array
+     * @return Builder|Collection|mixed
      */
-    public static function getKeywords(int $chat_id): array
+    public static function getKeywords(int $chat_id): mixed
     {
         $data = Cache::get("DB::TChatKeywords::chat_keywords::{$chat_id}");
-        if (is_array($data)) {
+        if ($data) {
             return $data;
         }
         $data = self::query()
             ->select('keyword', 'target', 'operation', 'data')
             ->where('chat_id', $chat_id)
             ->where('enable', 1)
-            ->get()
-            ->toArray();
+            ->get();
         Cache::put("DB::TChatKeywords::chat_keywords::{$chat_id}", $data, Carbon::now()->addMinutes(5));
         return $data;
     }
--- app/Services/Keywords/KeywordDetectKeyword.php
+++ app/Services/Keywords/KeywordDetectKeyword.php
@@ -2,10 +2,16 @@
 
 namespace App\Services\Keywords;
 
+use App\Exceptions\Handler;
+use App\Jobs\DeleteMessageJob;
+use App\Jobs\SendMessageJob;
 use App\Models\TChatKeywords;
+use App\Models\TChatKeywordsOperationEnum;
+use App\Models\TChatKeywordsTargetEnum;
 use App\Services\Base\BaseKeyword;
 use Longman\TelegramBot\Entities\Message;
 use Longman\TelegramBot\Telegram;
+use Throwable;
 
 class KeywordDetectKeyword extends BaseKeyword
 {
@@ -20,21 +26,139 @@ class KeywordDetectKeyword extends BaseKeyword
 
     public function execute(Message $message, Telegram $telegram, int $updateId): void
     {
+        /** @var TChatKeywords[] $keywords */
         $keywords = TChatKeywords::getKeywords($message->getChat()->getId());
         foreach ($keywords as $keyword) {
-            $this->handle(
-                $keyword['keyword'],
-                $keyword['target'],
-                $keyword['operation'],
-                $keyword['data'],
-                $message,
-                $telegram,
-                $updateId
-            );
+            try {
+                $this->handle($keyword->keyword, $keyword->target, $keyword->operation, $keyword->data, $message, $telegram, $updateId);
+            } catch (Throwable $e) {
+                Handler::logError($e);
+            }
         }
     }
 
-    private function handle(string $keyword, string $target, string $operation, array $data, Message $message, Telegram $telegram, int $updateId)
+    private function handle(
+        string                     $keyword,
+        TChatKeywordsTargetEnum    $target,
+        TChatKeywordsOperationEnum $operation,
+        array                      $data,
+        Message                    $message, Telegram $telegram, int $updateId
+    )
+    {
+        switch ($target) {
+            case TChatKeywordsTargetEnum::TARGET_CHATID:
+                $chatId = $message->getChat()->getId();
+                if ($chatId == $keyword) {
+                    $this->runOperation($operation, $data, $message, $telegram, $updateId);
+                }
+                break;
+            case TChatKeywordsTargetEnum::TARGET_USERID:
+                $userId = $message->getFrom()->getId();
+                if ($userId == $keyword) {
+                    $this->runOperation($operation, $data, $message, $telegram, $updateId);
+                }
+                break;
+            case TChatKeywordsTargetEnum::TARGET_NAME:
+                $name = ($message->getFrom()->getFirstName() ?? '') . ($message->getFrom()->getLastName() ?? '');
+                if (str_contains($name, $keyword)) {
+                    $this->runOperation($operation, $data, $message, $telegram, $updateId);
+                }
+                break;
+            case TChatKeywordsTargetEnum::TARGET_FROMNAME:
+                $fromName = ($message->getForwardFrom()->getFirstName() ?? '') . ($message->getForwardFrom()->getLastName() ?? '');
+                if (str_contains($fromName, $keyword)) {
+                    $this->runOperation($operation, $data, $message, $telegram, $updateId);
+                }
+                break;
+            case TChatKeywordsTargetEnum::TARGET_TITLE:
+                $title = $message->getForwardFromChat()->getTitle();
+                if (str_contains($title, $keyword)) {
+                    $this->runOperation($operation, $data, $message, $telegram, $updateId);
+                }
+                break;
+            case TChatKeywordsTargetEnum::TARGET_TEXT:
+                $text = $message->getText() ?? $message->getCaption() ?? '';
+                if (str_contains($text, $keyword)) {
+                    $this->runOperation($operation, $data, $message, $telegram, $updateId);
+                }
+                break;
+        }
+    }
+
+    private function runOperation(
+        TChatKeywordsOperationEnum $operation,
+        array                      $data,
+        Message                    $message, Telegram $telegram, int $updateId
+    )
+    {
+        switch ($operation) {
+            case TChatKeywordsOperationEnum::OPERATION_REPLY:
+                $this->reply($data, $message, $telegram, $updateId);
+                break;
+            case TChatKeywordsOperationEnum::OPERATION_DELETE:
+                $this->delete($data, $message, $telegram, $updateId);
+                break;
+            case TChatKeywordsOperationEnum::OPERATION_WARN:
+                $this->warn($data, $message, $telegram, $updateId);
+                break;
+            case TChatKeywordsOperationEnum::OPERATION_BAN:
+                $this->ban($data, $message, $telegram, $updateId);
+                break;
+            case TChatKeywordsOperationEnum::OPERATION_RESTRICT:
+                $this->restrict($data, $message, $telegram, $updateId);
+                break;
+            case TChatKeywordsOperationEnum::OPERATION_FORWARD:
+                $this->forward($data, $message, $telegram, $updateId);
+                break;
+            case TChatKeywordsOperationEnum::OPERATION_REPEAT:
+                $this->repeat($data, $message, $telegram, $updateId);
+                break;
+        }
+    }
+
+    private function reply(array $data, Message $message, Telegram $telegram, int $updateId)
+    {
+        $sender = [
+            'chat_id' => $message->getChat()->getId(),
+            'reply_to_message_id' => $message->getMessageId(),
+        ];
+    }
+
+    private function delete(array $data, Message $message, Telegram $telegram, int $updateId)
+    {
+        $deleter = [
+            'chat_id' => $message->getChat()->getId(),
+            'message_id' => $message->getMessageId(),
+        ];
+        $this->dispatch(new DeleteMessageJob($deleter, 0));
+        $sender = [
+            'chat_id' => $message->getChat()->getId(),
+        ];
+        isset($data['text']) && $sender['text'] = $data['text'];
+        count($sender) > 1 && $this->dispatch(new SendMessageJob($sender));
+    }
+
+    private function warn(array $data, Message $message, Telegram $telegram, int $updateId)
+    {
+
+    }
+
+    private function ban(array $data, Message $message, Telegram $telegram, int $updateId)
+    {
+
+    }
+
+    private function restrict(array $data, Message $message, Telegram $telegram, int $updateId)
+    {
+
+    }
+
+    private function forward(array $data, Message $message, Telegram $telegram, int $updateId)
+    {
+
+    }
+
+    private function repeat(array $data, Message $message, Telegram $telegram, int $updateId)
     {
 
     }
