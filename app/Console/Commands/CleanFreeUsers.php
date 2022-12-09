20985f8e01af6076ce007741d5276f58e7f152db
jyxjjj <773933146@qq.com>
2022-12-09 22:57:38 +0800

    CleanFreeUsers

---
+++ app/Console/Commands/CleanFreeUsers.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Common\Conversation;
+use Illuminate\Console\Command;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use RegexIterator;
+
+class CleanFreeUsers extends Command
+{
+    protected $signature = 'command:CleanFreeUsers';
+    protected $description = 'Clean Free Users';
+
+    /**
+     * @return int
+     */
+    public function handle(): int
+    {
+        $path = storage_path('app/telegram/conversation/contribute');
+        $files = new RegexIterator(
+            new RecursiveIteratorIterator(
+                new RecursiveDirectoryIterator($path)
+            ),
+            '/^.+\.json$/i'
+        );
+        foreach ($files as $file) {
+            $filename = $file->getFilename();
+            $userId = str_replace('.json', '', $filename);
+            $userData = Conversation::get($userId, 'contribute');
+            if (count($userData) == 0) {
+                continue;
+            }
+            if ($userData === ['status' => 'free']) {
+                $this->info($filename);
+                unlink($file->getPathname());
+            }
+        }
+        return self::SUCCESS;
+    }
+}
