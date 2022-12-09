48449b478808364f3b941624e328eab87e1440d2
jyxjjj <773933146@qq.com>
2022-12-09 23:23:59 +0800

    Create FixLinks.php

---
+++ app/Console/Commands/FixLinks.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Common\Conversation;
+use Illuminate\Console\Command;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use RegexIterator;
+
+class FixLinks extends Command
+{
+    protected $signature = 'command:FixLinks';
+    protected $description = '';
+
+    /**
+     * @return int
+     */
+    public function handle(): int
+    {
+        $path = storage_path('app/telegram/conversation/contribute');
+        $files = new RegexIterator(
+            new RecursiveIteratorIterator(
+                new RecursiveDirectoryIterator($path)
+            ),
+            '/^.+\.json$/i'
+        );
+        foreach ($files as $file) {
+            $filename = $file->getFilename();
+            $userId = str_replace('.json', '', $filename);
+            $userData = Conversation::get($userId, 'contribute');
+            foreach ($userData as $cvid => $cvinfo) {
+                if (str_starts_with($cvid, '16')) {
+                    if ($cvinfo['status'] == 'pass') {
+                        $this->info($cvid);
+                        $link = $cvinfo['link'];
+                        $newLinks = Conversation::get('link', 'link');
+                        $newLinks[$cvid] = $link;
+                        Conversation::save('link', 'link', $newLinks);
+                    }
+                }
+            }
+        }
+        return self::SUCCESS;
+    }
+}
