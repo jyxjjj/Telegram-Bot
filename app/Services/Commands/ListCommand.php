e8cdda171f47a1e3b67d599aefa6adb43216249f
jyxjjj <773933146@qq.com>
2022-12-09 23:48:11 +0800

    Update ListCommand.php

--- app/Services/Commands/ListCommand.php
+++ app/Services/Commands/ListCommand.php
@@ -33,11 +33,7 @@ class ListCommand extends BaseCommand
         ];
         $pendingData = Conversation::get('pending', 'pending');
         foreach ($pendingData as $cvid => $user_id) {
-            $userData = Conversation::get($user_id, 'contribute');
-            $cvinfo = $userData[$cvid];
-            $name = $cvinfo['name'];
-            $link = $cvinfo['link'];
-            $data['text'] .= "投稿ID: <code>$cvid</code>\n用户ID: <code>$user_id</code>\n投稿名称: <code>$name</code>\n投稿链接: <code>$link</code>\n\n";
+            $data['text'] .= "<code>$cvid</code>\n";
         }
         $data['text'] .= '请根据信息搜索聊天记录';
         $this->dispatch(new SendMessageJob($data, null, 0));
