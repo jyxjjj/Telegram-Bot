7cf67399be94b3245b967f9ccbffc79b01edfa40
jyxjjj <773933146@qq.com>
2022-12-09 14:36:57 +0800

    Update StartCommand.php

--- app/Services/Commands/StartCommand.php
+++ app/Services/Commands/StartCommand.php
@@ -34,6 +34,10 @@ class StartCommand extends BaseCommand
         ];
         $data['text'] .= "Hello, I am here alive.\n";
         $data['text'] .= "Type /help to get the help.\n";
+        $data['text'] .= "Type /about to get the open source information, Privacy Policies, Usage Agreements.\n";
+        $data['text'] .= "This bot is none of any of your groups' business, it is free for all and can be set by third parties to do anything it can do.\n";
+        $data['text'] .= "We do not provide any security promises and data keeps.\n";
+        $data['text'] .= "Any questions, please contact @jyxjjj .\n";
         $data['text'] .= "<b>Your user_id</b>: <a href='tg://user?id=$startedUser->user_id'>$startedUser->user_id</a>\n";
         $payload && $data['text'] .= "<b>Your payload</b>: <code>$payload</code>\n";
         $this->dispatch(new SendMessageJob($data, null, 0));
