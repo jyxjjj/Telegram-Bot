69604965ad0b813ffb317f074cf88440f21da1bb
jyxjjj <773933146@qq.com>
2022-12-09 14:43:55 +0800

    Update StartCommand.php

--- app/Services/Commands/StartCommand.php
+++ app/Services/Commands/StartCommand.php
@@ -34,7 +34,7 @@ class StartCommand extends BaseCommand
         ];
         $data['text'] .= "Hello, I am here alive.\n";
         $data['text'] .= "Type /help to get the help.\n";
-        $data['text'] .= "Type /about to get the open source information, Privacy Policies, Usage Agreements.\n";
+        $data['text'] .= "Type /about to get the open source information, privacy policies, usage agreements.\n";
         $data['text'] .= "This bot is none of any of your groups' business, it is free for all and can be set by third parties to do anything it can do.\n";
         $data['text'] .= "We do not provide any security promises and data keeps.\n";
         $data['text'] .= "Any questions, please contact @jyxjjj .\n";
