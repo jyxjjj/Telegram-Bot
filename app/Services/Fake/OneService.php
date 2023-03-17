f67c36d929ce4fa5f5da16c8d64e73459940ca30
jyxjjj <773933146@qq.com>
2023-03-17 09:46:41 +0800

    2023-03-17 09:46:41

---
+++ app/Services/Fake/OneService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Services\Fake;
+
+class OneService
+{
+    // asd
+    public function __construct()
+    {
+    }
+
+    // asd
+    public function get()
+    {
+        return 'asd';
+    }
+
+    // asd
+    public function get2()
+    {
+        return 'asd';
+    }
+
+    public function get3()
+    {
+        return 'asd';
+    }
+}
---
+++ app/Services/Fake/TwoService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services\Fake;
+
+class TwoService
+{
+    // asd
+    public function __construct()
+    {
+    }
+}
