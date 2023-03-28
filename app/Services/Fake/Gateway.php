1463bea974af2f97b5d6da29df664a3a2b7accb2
jyxjjj <773933146@qq.com>
2023-03-28 14:47:05 +0800

    Create Gateway.php

---
+++ app/Services/Fake/Gateway.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Services\Fake;
+
+class Gateway
+{
+    private string $apiKey;
+    private string $apiSecret;
+
+    public function __construct(string $apiKey, string $apiSecret)
+    {
+        $this->apiKey = $apiKey;
+        $this->apiSecret = $apiSecret;
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiKey(): string
+    {
+        return $this->apiKey;
+    }
+
+    /**
+     * @param string $apiKey
+     */
+    public function setApiKey(string $apiKey): void
+    {
+        $this->apiKey = $apiKey;
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiSecret(): string
+    {
+        return $this->apiSecret;
+    }
+
+    /**
+     * @param string $apiSecret
+     */
+    public function setApiSecret(string $apiSecret): void
+    {
+        $this->apiSecret = $apiSecret;
+    }
+
+}
