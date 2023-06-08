e329d28554ff0074a653d295bcc4385aaba0a104
jyxjjj <773933146@qq.com>
2023-06-08 12:24:31 +0800

    Create IATACommand.php

---
+++ app/Services/Commands/IATACommand.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Services\Commands;
+
+use App\Jobs\SendMessageJob;
+use App\Services\Base\BaseCommand;
+use Longman\TelegramBot\Entities\Message;
+use Longman\TelegramBot\Telegram;
+
+class IATACommand extends BaseCommand
+{
+    public string $name = 'iata';
+    public string $description = 'Search iata info.';
+    public string $usage = '/iata [Search String]';
+
+    /**
+     * @param Message $message
+     * @param Telegram $telegram
+     * @param int $updateId
+     * @return void
+     */
+    public function execute(Message $message, Telegram $telegram, int $updateId): void
+    {
+        $chatId = $message->getChat()->getId();
+        $messageId = $message->getMessageId();
+        $params = $message->getText(true);
+        $data = [
+            'chat_id' => $chatId,
+            'reply_to_message_id' => $messageId,
+            'text' => $this->find(trim($params)),
+        ];
+        $this->dispatch(new SendMessageJob($data, null, 0));
+    }
+
+    private function find(string $name): string
+    {
+        $exists = file_exists(database_path('airports.json'));
+        if (!$exists) {
+            return "<b>ERROR</b>: Cannot found airports database.";
+        }
+        $file = file_get_contents(database_path('airports.json'));
+        $json = json_decode($file, true);
+        unset($file);
+        foreach ($json as $id => $airport) {
+            if (str_contains(strtolower($airport['iata']), strtolower($name))) {
+                return <<<EOF
+Name: {$airport['name']}
+IATA: {$airport['iata']}
+ICAO: {$airport['icao']}
+Place: {$airport['city']}, {$airport['state']}, {$airport['country']}
+Location: {$airport['lat']}, {$airport['lon']}, {$airport['elevation']}
+Timezone: {$airport['tz']}
+EOF;
+            }
+        }
+        return "<b>ERROR</b>: Cannot found airport which iata contains '$name'.";
+    }
+}
