d3f55ecb00574c1c4e139234b4ed6899cf94b827
jyxjjj <773933146@qq.com>
2023-07-25 10:56:34 +0800

    Create BV2AVCommand.php

---
+++ app/Services/Commands/BV2AVCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Services\Commands;
+
+use App\Jobs\SendMessageJob;
+use App\Services\Base\BaseCommand;
+use Longman\TelegramBot\Entities\Message;
+use Longman\TelegramBot\Telegram;
+
+class BV2AVCommand extends BaseCommand
+{
+    public string $name = 'bv2av';
+    public string $description = 'BV to AV';
+    public string $usage = '/bv2av [BVID]';
+    private int $a2bAddEnc = 8728348608;
+    private int $a2bXorEnc = 0b1010100100111011001100100100;
+    private array $a2bEncIndex = [11, 10, 3, 8, 4, 6];
+    private string $a2bEncTable = "fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF";
+
+    /**
+     * @param Message $message
+     * @param Telegram $telegram
+     * @param int $updateId
+     * @return void
+     */
+    public function execute(Message $message, Telegram $telegram, int $updateId): void
+    {
+        $chatId = $message->getChat()->getId();
+        $param = $message->getText(true);
+        $data = [
+            'chat_id' => $chatId,
+            'text' => $this->bv2av($param),
+        ];
+        $data['text'] && $this->dispatch(new SendMessageJob($data, null, 0));
+    }
+
+    private function bv2av(string $bv): string
+    {
+        $temp = 0;
+        for ($i = 0; $i < count($this->a2bEncIndex); $i++) {
+            if (!str_contains($this->a2bEncTable, $bv[$this->a2bEncIndex[$i]])) {
+                return 'Invaild BV ID.';
+            } else {
+                $temp += strpos($this->a2bEncTable, $bv[$this->a2bEncIndex[$i]]) * pow(strlen($this->a2bEncTable), $i);
+            }
+        }
+        $temp = $temp - $this->a2bAddEnc ^ $this->a2bXorEnc;
+        return 'av' . $temp;
+    }
+}
