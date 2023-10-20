9455e711ccfc0ea6ef5430c37a0d6c26ca3bd60d
jyxjjj <773933146@qq.com>
2023-10-20 11:30:22 +0800

    Update TrustProxies.php

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -16,27 +16,27 @@ class TrustProxies extends Middleware
         '192.168.0.0/16',
 
         // https://www.cloudflare.com/ips-v4
-        '173.245.48.0/20', // CloudFlare
         '103.21.244.0/22', // CloudFlare
         '103.22.200.0/22', // CloudFlare
         '103.31.4.0/22', // CloudFlare
-        '141.101.64.0/18', // CloudFlare
-        '108.162.192.0/18', // CloudFlare
-        '190.93.240.0/20', // CloudFlare
-        '188.114.96.0/20', // CloudFlare
-        '197.234.240.0/22', // CloudFlare
-        '198.41.128.0/17', // CloudFlare
-        '162.158.0.0/15', // CloudFlare
         '104.16.0.0/13', // CloudFlare
         '104.24.0.0/14', // CloudFlare
-        '172.64.0.0/13', // CloudFlare
+        '108.162.192.0/18', // CloudFlare
         '131.0.72.0/22', // CloudFlare
+        '141.101.64.0/18', // CloudFlare
+        '162.158.0.0/15', // CloudFlare
+        '172.64.0.0/13', // CloudFlare
+        '173.245.48.0/20', // CloudFlare
+        '188.114.96.0/20', // CloudFlare
+        '190.93.240.0/20', // CloudFlare
+        '197.234.240.0/22', // CloudFlare
+        '198.41.128.0/17', // CloudFlare
         // https://www.cloudflare.com/ips-v6
         '2400:cb00::/32', // CloudFlare
+        '2405:8100::/32', // CloudFlare
+        '2405:b500::/32', // CloudFlare
         '2606:4700::/32', // CloudFlare
         '2803:f800::/32', // CloudFlare
-        '2405:b500::/32', // CloudFlare
-        '2405:8100::/32', // CloudFlare
         '2a06:98c0::/29', // CloudFlare
         '2c0f:f248::/32', // CloudFlare
     ];
