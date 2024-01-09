8a344de06b5b07b20542641152c8818924b6d9d3
jyxjjj <773933146@qq.com>
2024-01-09 09:53:35 +0800

    Update routes.php

--- app/Providers/routes.php
+++ app/Providers/routes.php
@@ -2,12 +2,14 @@
 
 use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 Route::group(
     [
     ],
     function () {
         Route::get('/', [IndexController::class, 'index']);
+        Route::get('/generate_204', fn() => response(null, Response::HTTP_NO_CONTENT)); // Captive Portal Detection
     }
 );
 
