9082a708ec3b13253a69de2c6e1839b8605c5500
jyxjjj <773933146@qq.com>
2024-02-07 09:11:54 +0800

    Update B23.php

--- app/Common/B23.php
+++ app/Common/B23.php
@@ -2,15 +2,20 @@
 
 namespace App\Common;
 
-final class B23
+final readonly class B23
 {
-    const int a2bAddEnc = 0b1000001000010000000000011111000000;
-    const int a2bXorEnc = 0b1010100100111011001100100100;
-    const array a2bEncIndex = [11, 10, 3, 8, 4, 6];
-    const string a2bEncTable = "fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF";
-    const int a2bEncTableLength = 58;
-    const int a2bEncIndexLength = 6;
+    private const int a2bAddEnc = 0b1000001000010000000000011111000000;
+    private const int a2bXorEnc = 0b1010100100111011001100100100;
+    private const array a2bEncIndex = [11, 10, 3, 8, 4, 6];
+    private const string a2bEncTable = "fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF";
+    private const int a2bEncTableLength = 58;
+    private const int a2bEncIndexLength = 6;
 
+    /**
+     * @param string $av
+     * @return string
+     * @deprecated AVID larger than 2^27 will cause issue.
+     */
     public static function AV2BV(string $av): string
     {
         str_starts_with($av, 'av') && $av = substr($av, 2);
@@ -33,6 +38,11 @@ final class B23
         return $temp;
     }
 
+    /**
+     * @param string $bv
+     * @return string
+     * @deprecated AVID larger than 2^27 will cause issue.
+     */
     public static function BV2AV(string $bv): string
     {
         $temp = 0;
