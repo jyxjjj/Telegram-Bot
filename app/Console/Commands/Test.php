1a3358823bf3912d58ceb0267d1fdc335d7002d3
jyxjjj <773933146@qq.com>
2024-02-07 09:12:20 +0800

    AVID > 2 ^ 27

--- app/Console/Commands/Test.php
+++ app/Console/Commands/Test.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Common\B23;
 use Illuminate\Console\Command;
 
 class Test extends Command
@@ -11,6 +12,7 @@ class Test extends Command
 
     public function handle(): int
     {
+        dump(B23::BV2AV('BV1Ty421Y7gV') == 'av1550274374', B23::AV2BV('av1550274374') == 'BV1Ty421Y7gV');
         return self::SUCCESS;
     }
 }
