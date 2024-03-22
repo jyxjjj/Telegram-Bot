b2c118d88a08f0fe6013f0f534e59a3fbe64da4c
jyxjjj <773933146@qq.com>
2024-03-22 11:57:47 +0800

    fix default queue name "default" instead of "queue"

--- config/queue.php
+++ config/queue.php
@@ -5,7 +5,7 @@ return [
         'redis' => [
             'driver' => 'redis',
             'connection' => 'queue',
-            'queue' => 'queue',
+            'queue' => 'default',
             'retry_after' => 60,
             'block_for' => null,
             'after_commit' => false,
