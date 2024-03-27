55b4cbf2835db5bc63bfea3a9f7123876c3d01c1
jyxjjj <773933146@qq.com>
2024-03-27 13:17:13 +0800

    Go

--- app/Console/Schedule/WellKnownSoftwareUpdateSubscribe.php
+++ app/Console/Schedule/WellKnownSoftwareUpdateSubscribe.php
@@ -15,7 +15,7 @@ class WellKnownSoftwareUpdateSubscribe extends Command
 {
     use DispatchesJobs;
 
-    protected $signature = 'subscribe:update';
+    protected $signature = 'subscribe:update {--s|software=} {software?}';
     protected $description = 'Get the Newest Version then push to target chat';
 
     public function __construct()
@@ -26,51 +26,62 @@ class WellKnownSoftwareUpdateSubscribe extends Command
     public function handle(): int
     {
         try {
-            foreach (Software::cases() as $software) {
-                self::info("Checking $software->name...");
-                if (!in_array($software->name, [
-                    Software::PHP->name,
-                    Software::Nginx->name,
-                    Software::MariaDB->name,
-                    Software::MariaDBDocker->name,
-                    Software::Redis->name,
-                    Software::RedisDocker->name,
-                    Software::NodeJS->name,
-                    Software::Kernel->name,
-                    Software::KernelFeodra->name,
-                    Software::OpenSSL->name,
-                    Software::Laravel->name,
-                    Software::VSCode->name,
-                    Software::CURL->name,
-                    Software::NVM->name,
-                ])) {
-                    self::warn("Skip $software->name");
-                    continue;
-                }
-                try {
-                    /** @var TUpdateSubscribes[] $datas */
-                    $datas = TUpdateSubscribes::getAllSubscribeBySoftware($software->name);
-                    foreach ($datas as $data) {
-                        $chat_id = $data['chat_id'];
-                        $version = $software->getInstance()->getVersion();
-                        $lastVersion = Common::getLastVersion($software);
-                        self::info("$software->name Current:$version Latest:$lastVersion");
-                        if ($version && $lastVersion != $version) {
-                            $message = $software->getInstance()->generateMessage($chat_id, $version);
-                            $this->dispatch(new SendMessageJob($message, null, 0));
-                            Common::setLastVersion($software, $version);
-                        }
+            if ($this->option('software') || $this->argument('software')) {
+                $software = Software::from($this->option('software') ?? $this->argument('software'));
+                $this->dealCheck($software);
+            } else {
+                foreach (Software::cases() as $software) {
+                    self::info("Checking $software->name...");
+                    if (!in_array($software->name, [
+                        Software::PHP->name,
+                        Software::Nginx->name,
+                        Software::MariaDB->name,
+                        Software::MariaDBDocker->name,
+                        Software::Redis->name,
+                        Software::RedisDocker->name,
+                        Software::NodeJS->name,
+                        Software::Kernel->name,
+                        Software::KernelFeodra->name,
+                        Software::OpenSSL->name,
+                        Software::Laravel->name,
+                        Software::VSCode->name,
+                        Software::CURL->name,
+                        Software::NVM->name,
+                        Software::Go->name,
+                    ])) {
+                        self::warn("Skip $software->name");
+                        continue;
                     }
-                } catch (Throwable $e) {
-                    ERR::log($e);
-                    continue;
+                    try {
+                        $this->dealCheck($software);
+                    } catch (Throwable $e) {
+                        ERR::log($e);
+                        continue;
+                    }
+                    sleep(1);
                 }
-                sleep(1);
             }
             return self::SUCCESS;
         } catch (Throwable $e) {
             ERR::log($e);
             return self::FAILURE;
+        }
+    }
+
+    private function dealCheck(Software $software): void
+    {
+        /** @var TUpdateSubscribes[] $datas */
+        $datas = TUpdateSubscribes::getAllSubscribeBySoftware($software->name);
+        foreach ($datas as $data) {
+            $chat_id = $data['chat_id'];
+            $version = $software->getInstance()->getVersion();
+            $lastVersion = Common::getLastVersion($software);
+            self::info("$software->name Current:$version Latest:$lastVersion");
+            if ($version && $lastVersion != $version) {
+                $message = $software->getInstance()->generateMessage($chat_id, $version);
+                $this->dispatch(new SendMessageJob($message, null, 0));
+                Common::setLastVersion($software, $version);
+            }
         }
     }
 }
--- app/Console/Schedule/WellKnownSoftwareUpdateSubscribe/Softwares/Go.php
+++ app/Console/Schedule/WellKnownSoftwareUpdateSubscribe/Softwares/Go.php
@@ -2,12 +2,34 @@
 
 namespace App\Console\Schedule\WellKnownSoftwareUpdateSubscribe\Softwares;
 
+use App\Common\Config;
+use App\Console\Schedule\WellKnownSoftwareUpdateSubscribe\Common;
+use App\Console\Schedule\WellKnownSoftwareUpdateSubscribe\Software;
 use App\Console\Schedule\WellKnownSoftwareUpdateSubscribe\SoftwareInterface;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Http;
 use JetBrains\PhpStorm\ArrayShape;
 use Longman\TelegramBot\Entities\InlineKeyboard;
+use Longman\TelegramBot\Entities\InlineKeyboardButton;
 
 class Go implements SoftwareInterface
 {
+    const string GQL = <<<EOF
+{
+  repository(owner: "golang", name: "go") {
+    refs(
+      refPrefix: "refs/tags/"
+      last: 5
+      orderBy: {field: TAG_COMMIT_DATE, direction: ASC}
+    ) {
+      nodes {
+        name
+      }
+    }
+  }
+}
+EOF;
+
     /**
      * @param int $chat_id
      * @param string $version
@@ -20,7 +42,18 @@ class Go implements SoftwareInterface
     ])]
     public function generateMessage(int $chat_id, string $version): array
     {
-        // TODO: Implement generateMessage() method.
+        $emoji = Common::emoji();
+        $message = [
+            'chat_id' => $chat_id,
+            'text' => "$emoji A new version of Go($version) is now available.",
+            'reply_markup' => new InlineKeyboard([]),
+        ];
+        $button = new InlineKeyboardButton([
+            'text' => 'View',
+            'url' => 'https://github.com/golang/go',
+        ]);
+        $message['reply_markup']->addRow($button);
+        return $message;
     }
 
     /**
@@ -28,6 +61,49 @@ class Go implements SoftwareInterface
      */
     public function getVersion(): string
     {
-        // TODO: Implement getVersion() method.
+        $data = $this->getJson();
+        if (!is_array($data)) {
+            dump($data);
+            return Common::getLastVersion(Software::Go);
+        }
+        $major = 0;
+        $minor = 0;
+        $patch = 0;
+        $data = $data['data']['repository']['refs']['nodes'];
+        foreach ($data as $branch) {
+            if (preg_match('/^go(\d+)\.(\d+)\.(\d+)$/i', $branch['name'], $matches)) {
+                if ($matches[1] > $major) {
+                    $major = $matches[1];
+                    $minor = $matches[2];
+                    $patch = $matches[3];
+                } elseif ($matches[1] == $major && $matches[2] > $minor) {
+                    $minor = $matches[2];
+                    $patch = $matches[3];
+                } elseif ($matches[1] == $major && $matches[2] == $minor && $matches[3] > $patch) {
+                    $patch = $matches[3];
+                }
+            }
+        }
+        return "$major.$minor.$patch";
+    }
+
+    /**
+     * @return array|false
+     */
+    private function getJson(): array|false
+    {
+        $headers = Config::CURL_HEADERS;
+        $ts = Carbon::now()->getTimestamp();
+        $headers['User-Agent'] .= " Telegram-Go-Subscriber-Runner/$ts";
+        $get = Http::
+        withHeaders($headers)
+            ->withToken(env('GITHUB_TOKEN'))
+            ->post('https://api.github.com/graphql', [
+                'query' => self::GQL,
+            ]);
+        if ($get->status() == 200) {
+            return $get->json();
+        }
+        return false;
     }
 }
