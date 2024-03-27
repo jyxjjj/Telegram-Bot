92723245ac7ea28b3a8ea025b7f0af1cd84c0317
jyxjjj <773933146@qq.com>
2024-03-27 14:28:10 +0800

    Update AutoPassJob.php

--- app/Jobs/AutoPassJob.php
+++ app/Jobs/AutoPassJob.php
@@ -6,25 +6,20 @@ use App\Console\Commands\AutoPass;
 use App\Jobs\Base\BaseQueue;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Log;
 
 class AutoPassJob extends BaseQueue
 {
     public function handle(): void
     {
         if (Cache::has('autopass')) {
-            Log::debug('AutoPassJob0');
             return;
         }
         Cache::set('autopass', 1, 3600);
-        Log::debug('AutoPassJob1');
         Artisan::call(AutoPass::class);
-        Log::debug('AutoPassJob2');
         $data = [
             'chat_id' => env('YPP_SOURCE_ID'),
             'text' => '[SUCCESS]全部自动通过处理完成',
         ];
-        Log::debug('AutoPassJob3');
         SendMessageJob::dispatch($data, null, 0);
         Cache::delete('autopass');
     }
