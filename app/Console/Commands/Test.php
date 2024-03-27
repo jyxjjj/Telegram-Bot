ff9c519adc444bd02d3975217f19958a32b3fb75
jyxjjj <773933146@qq.com>
2024-03-27 12:05:58 +0800

    Update Test.php

--- app/Console/Commands/Test.php
+++ app/Console/Commands/Test.php
@@ -2,7 +2,6 @@
 
 namespace App\Console\Commands;
 
-use App\Common\B23;
 use Illuminate\Console\Command;
 
 class Test extends Command
@@ -12,13 +11,6 @@ class Test extends Command
 
     public function handle(): int
     {
-        dump(
-            'TEST',
-            B23::BV2AV('BV17x411w7KC'),
-            B23::BV2AV('BV17x411w7KC') == 'av170001',
-            B23::AV2BV('av170001'),
-            B23::AV2BV('av170001') == 'BV17x411w7KC'
-        );
         return self::SUCCESS;
     }
 }
