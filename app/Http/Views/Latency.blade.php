8590eb932122a53bd8af35a058415256019e80ef
jyxjjj <773933146@qq.com>
2024-06-06 16:07:12 +0800

    Update Latency.blade.php

--- app/Http/Views/Latency.blade.php
+++ app/Http/Views/Latency.blade.php
@@ -1,9 +1,11 @@
-<div id="main" style="width: 1024px; height: 512px;"></div>
-<a id="download" href="">Download AS PNG</a>
+<div id="main1" style="width: 1024px; height: 512px;"></div>
+<a id="download1" href="">Download AS PNG</a>
+<div id="main2" style="width: 1024px; height: 512px;"></div>
+<a id="download2" href="">Download AS PNG</a>
 <script src="https://unpkg.com/echarts@5.5.0/dist/echarts.js"></script>
 <script>
-    const charts = echarts.init(document.getElementById('main'));
-    const option = {
+    const charts1 = echarts.init(document.getElementById('main1'));
+    const option1 = {
         xAxis: {
             name: 'Time',
             type: 'category',
@@ -29,13 +31,56 @@
             large: true,
         }],
     };
-    charts.setOption(option);
-    charts.resize();
+    charts1.setOption(option1);
+    charts1.resize();
     window.onresize = function () {
-        charts.resize();
+        charts1.resize();
     };
-    image = charts.getDataURL();
-    download = document.getElementById('download');
+    image = charts1.getDataURL();
+    download = document.getElementById('download1');
+    download.href = image;
+    download.download = 'Latency.png';
+</script>
+<script>
+    const charts2 = echarts.init(document.getElementById('main2'));
+    const option2 = {
+        xAxis: {
+            name: 'Time',
+            type: 'category',
+            data: {!! $time !!},
+        },
+        yAxis: {
+            name: 'Rate',
+            type: 'value',
+            max: 100,
+            interval: 6.25,
+        },
+        grid: {
+            top: '72px',
+            left: '72px',
+            right: '72px',
+            bottom: '64px',
+        },
+        series: [{
+            name: 'Rate',
+            type: 'line',
+            data: {!! $data !!},
+            smooth: true,
+            large: true,
+        }],
+        dataZoom: [{
+            type: 'slider',
+            startValue: 0,
+            endValue: 100,
+        }]
+    };
+    charts2.setOption(option2);
+    charts2.resize();
+    window.onresize = function () {
+        charts2.resize();
+    };
+    image = charts2.getDataURL();
+    download = document.getElementById('download2');
     download.href = image;
     download.download = 'Latency.png';
 </script>
