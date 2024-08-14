171a2d331c637e6382b1cfa8a3aab633d36f6cb6
jyxjjj <773933146@qq.com>
2024-08-14 16:03:29 +0800

    Update DestinyCommand.php

--- app/Services/Commands/DestinyCommand.php
+++ app/Services/Commands/DestinyCommand.php
@@ -6,7 +6,6 @@ use App\Jobs\SendMessageJob;
 use App\Services\Base\BaseCommand;
 use Longman\TelegramBot\Entities\InlineKeyboard;
 use Longman\TelegramBot\Entities\InlineKeyboardButton;
-use Longman\TelegramBot\Entities\LoginUrl;
 use Longman\TelegramBot\Entities\Message;
 use Longman\TelegramBot\Telegram;
 
@@ -15,25 +14,23 @@ class DestinyCommand extends BaseCommand
     public string $name = 'destinylogin';
     public string $description = 'Login your bungie account';
     public string $usage = '/destinylogin';
+    public bool $private = true;
 
     public function execute(Message $message, Telegram $telegram, int $updateId): void
     {
         $chatId = $message->getChat()->getId();
         $messageId = $message->getMessageId();
-        $text = $message->getText() ?? '';
         $data = [
             'chat_id' => $chatId,
             'reply_to_message_id' => $messageId,
-            'text' => 'Please click this button to bind or rebind your Bungie Account.',
+            'text' => '',
             'protect_content' => true,
             'reply_markup' => new InlineKeyboard([]),
         ];
+        $data['text'] .= 'Please click this button to bind your Bungie Account.';
         $loginButton = new InlineKeyboardButton([
             'text' => 'Bind Your Bungie Account',
-            'login_url' => new LoginUrl([
-                'url' => 'https://tgapi.desmg.org/api/bungie/oauth/login',
-                'request_write_access' => true,
-            ]),
+            'url' => 'https://tgapi.desmg.org/api/bungie/oauth/login?user_id=' . $chatId,
         ]);
         $data['reply_markup']->addRow($loginButton);
         $this->dispatch(new SendMessageJob($data, null, 0));
