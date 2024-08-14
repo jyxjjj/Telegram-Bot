5384cf9423dd6e98b31dc803c6b156de6857e870
jyxjjj <773933146@qq.com>
2024-08-14 16:03:31 +0800

    Update BungieController.php

--- app/Http/Controllers/BungieController.php
+++ app/Http/Controllers/BungieController.php
@@ -2,17 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class BungieController extends BaseController
 {
-    public function login(Request $request): void
+    public function login(Request $request): string|RedirectResponse
     {
-        dump($request->query());
+        //  BUNGIE_OAUTH_URL=https://www.bungie.net/en/OAuth/Authorize
+        $user_id = $request->query('user_id') ?? '';
+        if (!is_numeric($user_id)) {
+            return 'Invalid user';
+        }
     }
 
     public function redirect(Request $request): void
     {
-        dump($request->query());
+        dump($request->all());
     }
 }
