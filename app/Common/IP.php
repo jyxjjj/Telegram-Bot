bf16b18df0f65ef772cd5463d0b346f1ffe838bf
jyxjjj <773933146@qq.com>
2025-10-31 11:37:58 +0800

    Update IP.php

--- app/Common/IP.php
+++ app/Common/IP.php
@@ -36,57 +36,32 @@ final class IP
 {
     public static function getClientIpInfos(): ?string
     {
-        $ip = self::getClientIp();
-        $country = self::getClientIpCountry();
-        $continent = self::getClientIpContinent();
-        $city = self::getClientIpCity();
-        $lat = self::getClientIpLatitude();
-        $lon = self::getClientIpLongitude();
-        return
-            !$country ?
-                $ip :
-                (
-                !$continent ?
-                    "$ip ($country)" :
-                    (
-                    !$city ?
-                        "$ip ($continent, $country)" :
-                        (
-                        !$lat || !$lon ?
-                            "$ip ($city, $continent, $country)" :
-                            "$ip ($city, $continent, $country) [$lat, $lon]"
-                        )
-                    )
-                );
+        $request = request();
+        $clientIp = $request->header('cf-connecting-ip', $request->getClientIp());
+        $continent = $request->header('cf-ipcontinent');
+        $country = $request->header('cf-ipcountry');
+        $regionCode = $request->header('cf-region-code');
+        $region = $request->header('cf-region');
+        $city = $request->header('cf-ipcity');
+        $latitude = $request->header('cf-iplatitude');
+        $longitude = $request->header('cf-iplongitude');
+        if ($city && $region && $regionCode && $country && $continent && $latitude && $longitude) {
+            return sprintf('%s (%s, %s) (%s, %s, %s) [%s, %s]', $clientIp, $city, $region, $regionCode, $country, $continent, $latitude, $longitude);
+        } elseif ($city && $region && $regionCode && $country && $continent) {
+            return sprintf('%s (%s, %s) (%s, %s, %s)', $clientIp, $city, $region, $regionCode, $country, $continent);
+        } elseif ($regionCode && $country && $continent) {
+            return sprintf('%s (%s, %s, %s)', $clientIp, $regionCode, $country, $continent);
+        } elseif ($country && $continent) {
+            return sprintf('%s (%s, %s)', $clientIp, $country, $continent);
+        } elseif ($country) {
+            return sprintf('%s (%s)', $clientIp, $country);
+        } else {
+            return $clientIp;
+        }
     }
 
     public static function getClientIp(): ?string
     {
         return request()->header('cf-connecting-ip', request()->getClientIp());
-    }
-
-    public static function getClientIpCountry(): ?string
-    {
-        return request()->header('cf-ipcountry');
-    }
-
-    public static function getClientIpContinent(): ?string
-    {
-        return request()->header('cf-ipcontinent');
-    }
-
-    public static function getClientIpCity(): ?string
-    {
-        return request()->header('cf-ipcity');
-    }
-
-    public static function getClientIpLatitude(): ?string
-    {
-        return request()->header('cf-iplatitude');
-    }
-
-    public static function getClientIpLongitude(): ?string
-    {
-        return request()->header('cf-iplongitude');
     }
 }
